25cbba4022de44537e4e7c496a58e08f550cb5a6
tarasov <a.a.tarasov@list.ru>
2022-06-22 13:01:56 +0300

    prepare

---
+++ src/Tool/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ToolComponent = ({ text }) => {
+  return <div>Tool Component: {text}</div>
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { ToolComponent } from './Tool'
+
+export { ToolComponent }
 
 export const ExampleComponent = ({ text }) => {
+  return <div className={styles.test}>Example Component: {text}</div>
+}
+
+export const AppComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
