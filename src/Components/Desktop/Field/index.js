9bdd8933b0131b85e75755341656b2601a5c26b6
tarasov <a.a.tarasov@list.ru>
2025-01-31 17:19:14 +0300

    1.1.300

--- src/Components/Desktop/Field/index.js
+++ src/Components/Desktop/Field/index.js
@@ -1167,8 +1167,6 @@ function BigObj({ formItem, auth, item, value, onChange, onAfterChange, changed,
     }, [meta, item]);
 
     const cRender = (auth, _item, value, onChange) => {
-        console.log(item);
-        
         return (<CollectionServer
             count={_item?.count}
             floatingFilter={item?.floatingFilter || item?.relation?.floatingFilter}
